60e79ed5e4bab47c7ac8c51e889b9b3d5f6dfdb6
LAKSHMI PRABHA K <prabhasanthiya2001@gmail.com>
2022-02-19 19:09:38 +0530

    commit and push

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { ContentComponent } from './content/content.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"profile",
+    component: ProfileComponent
+  },
+  {
+    path:"personal-info",
+    component: PersonalInfoComponent
+  },
+  {
+    path:"content",
+    component: ContentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
